b1d617b40331d4e70c716459b5208b4ff6d36a98
baorlys <lygiabaokg2002@gmail.com>
2024-03-12 15:27:03 +0700

    update UI, new feature project

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyA8pFP6ucyUyObIc3d2b9AJc6XQN15XHyM",
+    authDomain: "to-do-list-416304.firebaseapp.com",
+    projectId: "to-do-list-416304",
+    storageBucket: "to-do-list-416304.appspot.com",
+    messagingSenderId: "609057784235",
+    appId: "1:609057784235:web:dfa097d595e3762398d2ee",
+    measurementId: "G-5X5C05ZBWY",
+    vapidKey:"BO4rwu24AI2cmDAr8nSb_YpvsDeAo8xvyMX734wMY4-XbYHnw9GnuV9syX3wfYcLB3kt2eHswwJHSZ1QWSvmfE4"
+  }
+}
